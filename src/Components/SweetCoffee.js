f39b6a9775dbf7fab7185b79c6a380610c5f8b60
Jasper Wieringa <jasperwieringa@gmail.com>
2020-06-10 22:45:50 +0200

    Replaced the background image with the proper one from the assignment

--- src/Components/SweetCoffee.js
+++ src/Components/SweetCoffee.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-import Container from 'react-bootstrap/Container';
 import { css } from 'emotion';
+
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
 
 function SweetCoffee() {
   const styles = {
     container: css`
       height: 100%;
-    `
+    `,
   }
 
   return (
     <Container fluid className={styles.container}>
-      
     </Container>
   );
 }
