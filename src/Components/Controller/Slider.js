df74f72506e5b708368976802d7ed9c39467a97a
Jasper Wieringa <jasperwieringa@gmail.com>
2020-06-14 13:05:04 +0200

    Setup an overlay tooltip to the Slider component

--- src/Components/Controller/Slider.js
+++ src/Components/Controller/Slider.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 export default function Slider(props) {
   const [count, setCount] = useState(0);
@@ -13,17 +16,29 @@ export default function Slider(props) {
     props.handleSliderValues(props.name, value);
   }
 
+  const renderTooltip = (props) => {
+    return (
+      <Tooltip id="button-tooltip" {...props}>{count*10}%</Tooltip>
+    );
+  }
+
   return (
     <Col xs={6}>
       <Form.Label className="float-right">{props.name} [{props.stock[props.name]*10}%]</Form.Label>
-      <Form.Control
-        name={props.name}
-        value={count} 
-        type="range" 
-        max={props.stock[props.name]} 
-        step="1" 
-        disabled={props.stock[props.name] === 0} 
-        onChange={e => handleChange(e)}/>
+      <OverlayTrigger
+        placement="top"
+        delay={{ show: 10, hide: 200 }}
+        overlay={renderTooltip}
+      >
+        <Form.Control
+          name={props.name}
+          value={count} 
+          type="range" 
+          max={props.stock[props.name]} 
+          step="1" 
+          disabled={props.stock[props.name] === 0} 
+          onChange={e => handleChange(e)}/>
+      </OverlayTrigger>
     </Col>
   )
 }
